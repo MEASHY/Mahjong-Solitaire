3fe5d3621dea90f84af1e800d03ce3cc7068c26f
Stuart Bildfell <bildfells760@gmail.com>
2018-02-25 14:46:52 -0700

    Adding spacing per JS style guidelines: https://standardjs.com/rules.html

--- Unicog/unicog/app/Mahjong/Js/game.js
+++ Unicog/unicog/app/Mahjong/Js/game.js
@@ -10,7 +10,7 @@ var gameConfig = {
 
 }
 
-function preload() {
+function preload () {
     //In theory at this point we should have out tileset selected but for now we don't
     var session = new GameSession();
     session.tiles.main = [
@@ -43,21 +43,21 @@ function preload() {
     console.log("Assets loaded!")
 }
 
-function create() {
+function create () {
     console.log("creating!")
     this.board = new Board(this)
     console.log("Game created!")
 }
 
-function update() {
+function update () {
 }
 
-function startGame() {
+function startGame () {
     var game = new Phaser.Game(gameConfig)
     console.log(game)
 }
 
-function endGame() {
+function endGame () {
     // This will have to close the game when that is implemented, it's a placeholder for now.
     // This shouldn't be called when game.html is first loaded.
     // When this function is implemented that check should be done there or in this function, whatever works best.
