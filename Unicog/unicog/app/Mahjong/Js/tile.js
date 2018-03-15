dee3ea516f0fe5c3264d9dd50a9a085e90178595
Malcolm Fischer <mtfische@ualberta.ca>
2018-03-15 08:12:40 -0600

    Hints and Beginner Mode
    Hint Button disapears after a valid match. Beginner Mode now persists through shuffle.

--- Unicog/unicog/app/Mahjong/Js/tile.js
+++ Unicog/unicog/app/Mahjong/Js/tile.js
@@ -391,6 +391,11 @@ class Layout {
         
         this.generateTiles(counts, possible)
         this.positionSprites()
+        var s = new GameSession()
+        if (s.beginnerMode) {
+            this.initializeBeginnerMode()
+            console.log("starting beginner mode")
+        }
     }
 }
 
