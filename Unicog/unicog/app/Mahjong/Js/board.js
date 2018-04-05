f819a2f814bde9c4ff10c3d580020f9dad58ea2a
Eddy J. Tang <ejtang@ualberta.ca>
2018-04-05 17:03:28 -0600

    stuff is being added to the db, just have to tweek things now

--- Unicog/unicog/app/Mahjong/Js/board.js
+++ Unicog/unicog/app/Mahjong/Js/board.js
@@ -297,8 +297,46 @@ class Board {
         
         // Uses the continue button or finish button depending on if a game session has ended
         if (timerDone) {
-             this.scene.buttons.finish.toggleVisibility() 
+             this.scene.buttons.finish.toggleVisibility()
         } else {
+            var url = "http://localhost:5000/api/v1/create_mahjong_session";
+            var method = "POST";
+            var postData = JSON.stringify(gameStats)
+
+            // You REALLY want shouldBeAsync = true.
+            // Otherwise, it'll block ALL execution waiting for server response.
+            var shouldBeAsync = true;
+
+            var request = new XMLHttpRequest();
+
+            // Before we send anything, we first have to say what we will do when the
+            // server responds. This seems backwards (say how we'll respond before we send
+            // the request? huh?), but that's how Javascript works.
+            // This function attached to the XMLHttpRequest "onload" property specifies how
+            // the HTTP response will be handled. 
+            request.onload = function () {
+
+               // Because of javascript's fabulous closure concept, the XMLHttpRequest "request"
+               // object declared above is available in this function even though this function
+               // executes long after the request is sent and long after this function is
+               // instantiated. This fact is CRUCIAL to the workings of XHR in ordinary
+               // applications.
+
+               // You can get all kinds of information about the HTTP response.
+               var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+               var data = request.responseText; // Returned data, e.g., an HTML document.
+            }
+
+            request.open(method, url, shouldBeAsync);
+
+            request.setRequestHeader("JSON", "application/json;charset=UTF-8");
+            // Or... request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+            // Or... whatever
+
+            // Actually sends the request to the server.
+            request.send(postData);
+
+
              this.scene.buttons.next.toggleVisibility() 
         }
     }
